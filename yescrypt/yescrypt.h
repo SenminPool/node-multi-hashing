348cd07167462c62832ef0f475e6fb3f71a7d898
k725 <k725@users.noreply.github.com>
2018-02-03 14:57:54 +0900

    Add yescrypt_hash method in header.

--- yescrypt/yescrypt.h
+++ yescrypt/yescrypt.h
@@ -271,4 +271,9 @@ static int yescrypt_kdf(const yescrypt_shared_t * __shared,
     yescrypt_flags_t __flags,
     uint8_t * __buf, size_t __buflen);
 
+/**
+ * Interface
+ */
+void yescrypt_hash(const char *input, char *output);
+
 #endif /* !_YESCRYPT_H_ */
